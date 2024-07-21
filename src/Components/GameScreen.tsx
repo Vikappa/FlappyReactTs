5eab97ef283176823c797191cc6ff50ead1712f4
Vincenzo Costantini <54511489+Vikappa@users.noreply.github.com>
2024-07-21 12:43:45 +0200

    Velocità animazioni dipendente da variabile css modificabile

--- src/Components/GameScreen.tsx
+++ src/Components/GameScreen.tsx
@@ -1,29 +1,31 @@
-import { useEffect } from "react";
+import { useEffect, useState } from "react";
 
-const GameScreen = () =>{
+const GameScreen = () => {
+    const [animationSpeed, setAnimationSpeed] = useState(6); // Velocità di default
 
+    useEffect(() => {
+        const images = document.querySelectorAll('.backgroundImg');
+        images.forEach((image) => {
+            image.classList.add("movingBackground");
+        });
 
+        const roadImage = document.querySelector('#movingRoad');
+        if (roadImage) {
+            roadImage.classList.add("movingRoad");
+        }
 
-        useEffect(() => {
-            const images = document.querySelectorAll('.backgroundImg');
-            images.forEach((image) => {
-                image.classList.add("movingBackground")
-            })
+        document.documentElement.style.setProperty('--animation-speed', `${animationSpeed}s`);
+        document.documentElement.style.setProperty('--road-animation-speed', `${animationSpeed * 1.5}s`);
+    }, [animationSpeed]);
 
-            const roadImage = document.querySelector('#movingRoad');
-            if (roadImage) {
-                roadImage.classList.add("movingRoad")
-            }
-        }, []);
-
-    return(
+    return (
         <div id="game-screen">
-        <img src="/assets/img/background.png" className="backgroundImg" alt="background1" id="background1"/>
-        <img src="/assets/img/background.png" className="backgroundImg" alt="background2" id="background2"/>
-        <img src='/assets/img/154256.png' alt="road" className="movingRoad" id="movingRoad1" />
-        <img src='/assets/img/154256.png' alt="road" className="movingRoad" id="movingRoad2"/>
+            <img src="/assets/img/background.png" className="backgroundImg" alt="background1" id="background1" />
+            <img src="/assets/img/background.png" className="backgroundImg" alt="background2" id="background2" />
+            <img src='/assets/img/154256.png' alt="road" className="movingRoad" id="movingRoad1" />
+            <img src='/assets/img/154256.png' alt="road" className="movingRoad" id="movingRoad2" />
         </div>
-    )
+    );
 }
 
-export default GameScreen
+export default GameScreen;
